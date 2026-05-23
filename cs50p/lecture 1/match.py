4df06d96dc56e1cce0983e5b4cb133c407247274
harjashan-cs <harjashan2711@gmail.com>
2026-05-23 08:23:15 +0530

    Create match.py

---
+++ cs50p/lecture 1/match.py
@@ -0,0 +1,13 @@
+name = input("what is your name ?")
+
+match name :
+    case "jon snow" :
+        print("House Stark")
+    case "sansa" :
+        print("House Stark")
+    case "arya" :
+        print("House Stark")
+    case "jaime" :
+        print("House  Lannister")
+    case _:
+        print("who?")
