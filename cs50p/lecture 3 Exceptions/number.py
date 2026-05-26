2b1adbe3cbad2dfe5836f0eeb7947f12d48ca36c
harjashan-cs <harjashan2711@gmail.com>
2026-05-26 07:59:02 +0530

    Update number.py

--- cs50p/lecture 3 Exceptions/number.py
+++ cs50p/lecture 3 Exceptions/number.py
@@ -1,8 +1,14 @@
-while True:    
-    try :
-        x = int(input("what is x? "))
-        
-        break
-    except ValueError :
-        print("x is not a number")
-print("x is",x)
+def main() :
+    get_int()
+def get_int() :
+    while True:    
+        try :
+            x = int(input("what is x? "))
+            
+            break
+        except ValueError :
+            print("x is not a number")
+    print("x is",x)
+    44
+
+main()
